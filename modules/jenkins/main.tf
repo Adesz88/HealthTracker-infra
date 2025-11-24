e4bb96e40a80f60fccd41c0a36dceea98b2a6da3
Adam Olej <51245883+Adesz88@users.noreply.github.com>
2025-11-24 18:27:19 +0100

    Added jenkins

---
+++ modules/jenkins/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.0"
+    }
+  }
+}
+
+resource "docker_container" "jenkins" {
+  name = "jenkins"
+  image = "jenkins/jenkins:lts"
+
+  ports {
+    internal = 8080
+    external = 8080
+  }
+
+  ports {
+    internal = 50000
+    external = 50000
+  }
+
+  volumes {
+    host_path = "./jenkins_home"
+    container_path = "/var/jenkins_home"
+  }
+}
