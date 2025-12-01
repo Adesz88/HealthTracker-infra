a0c3e95fb35069ed2bc97a5a62243534493a6a86
Adam Olej <51245883+Adesz88@users.noreply.github.com>
2025-12-01 17:14:12 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Change the ownership of the Docker socket using sudo
-if [ -S /var/run/docker.sock ]; then
-    sudo chown root:docker /var/run/docker.sock
-fi
+#if [ -S /var/run/docker.sock ]; then
+#    sudo chown root:docker /var/run/docker.sock
+#fi
 
 # Execute the main container command
 exec "$@"
